f50f418d82ccd9becf309e2209f36462cf293eb5
M-Gabrova <milenagabrova@gmail.com>
2023-02-15 19:20:01 +0200

    Add project files.

---
+++ 04. Train The Trainers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();  
+
+            string presentationName;
+            int presentationCount = 0;
+            double grade = 0;
+            double studentGrade = 0;
+
+            while (input != "Finish")
+            {
+                presentationName = input;
+                grade = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    grade += double.Parse(Console.ReadLine());
+                }
+                grade = grade/n;
+                Console.WriteLine($"{presentationName} - {grade:f2}.");
+                studentGrade+= grade;
+                presentationCount++;
+
+                input= Console.ReadLine();
+            }
+
+            Console.WriteLine($"Student's final assessment is {studentGrade / presentationCount:f2}.");
+
+
+        }
+    }
+}
---
+++ izpitna-15-16-iuni-2019-z6-bileti-za-film/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            int studentTickets = 0;
+            int standardTickets = 0;
+            int kidTickets = 0;
+
+            while (movieName != "Finish")
+            {
+                int capacity = int.Parse(Console.ReadLine());
+
+                string ticketType = Console.ReadLine();
+                int seatsTaken = 0;
+
+                while (movieName != "End")
+                {
+                    ticketType = Console.ReadLine();
+
+                    if (ticketType == "student")
+                    {
+                        studentTickets++;
+                    }
+                    else if (ticketType == "standard")
+                    {
+                        standardTickets++;
+                    }
+                    else
+                    {
+                        kidTickets++;
+                    }
+
+                    seatsTaken++;
+
+                    if (seatsTaken == capacity)
+                    {
+                        break;
+                    }
+
+                    ticketType = Console.ReadLine();
+
+
+                }
+                double percentFull = (double)seatsTaken / capacity * 100;
+                Console.WriteLine($"{movieName} - {percentFull:F2}% full.");
+
+                movieName = Console.ReadLine();
+            }
+
+
+            int totalTickets = studentTickets + standardTickets + kidTickets;
+            Console.WriteLine($"Total tickets: {totalTickets}");
+
+            double studentPercent = (double)studentTickets / totalTickets * 100;
+            double standardPercent = (double)standardTickets / totalTickets * 100;
+            double kidPercent = (double)kidTickets / totalTickets * 100;
+            Console.WriteLine($"{studentPercent:F2}% student tickets.");
+            Console.WriteLine($"{standardPercent:F2}% standard tickets.");
+            Console.WriteLine($"{kidPercent:F2}% kids tickets.");
+        }
+    }
+
+}
